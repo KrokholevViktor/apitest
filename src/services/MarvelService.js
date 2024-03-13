a55fbb6c79775fe6bbcd499e2a75780b351b2c00
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-03-13 20:30:51 +0700

    service

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,7 +24,7 @@ class MarvelService {
         return this._transformCharacter(res.data.results[0]);
     }
 
-    _transformCharacter = (char) => {
+    _transformCharacter = (char) => { 
         return {
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
