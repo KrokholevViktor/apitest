f30c25c5d3e185c02cf3eb3cd270c01f83df14ee
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-03-15 19:56:19 +0700

    ref

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=979c9c76478e722c1913a4ab2b161e1e';
+    _baseOffset = 210;
 
 
     getResource = async (url) => {
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
