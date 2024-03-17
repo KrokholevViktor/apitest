4c9dc25941d03fe51d344e33ed85308fc7e17e17
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-03-17 15:05:33 +0700

    ComicsList

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import Spinner from '../spinner/Spinner';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './randomChar.scss';
@@ -10,37 +10,27 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
     const [char, setChar] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
-        updateChar()
+        updateChar();
+        const timerId = setInterval(updateChar, 60000);
+
+        return() => {
+            clearInterval(timerId)
+        }
     }, [])
     
     const updateChar = () => {
+        clearError();
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
-        onCharLoading();
-        marvelService
-        .getCharacter(id)
-        .then(onCharLoaded)
-        .catch(onError)
+        getCharacter(id)
+            .then(onCharLoaded)
     }
     
     const onCharLoaded = (char) => {
         setChar(char)
-        setLoading(false)
-    }
-
-    const onCharLoading = () => {
-        setLoading(true)
-        setError(false)
-    }
-
-    const onError = () => {
-        setLoading(false);
-        setError(true);
     }
 
     const errorMessage = error ? <ErrorMessage/> : null
